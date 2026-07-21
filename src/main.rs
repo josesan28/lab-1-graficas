2d004ce85c48eabed2c3d9969cd4444e2db0398c
josesan28 <san24092@uvg.edu.gt>
2026-07-21 14:40:29 -0600

    Polígono 4 añadido junto al agujero

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,44 @@ fn main() {
         Vector2::new(436.0, 249.0),
     ];
 
+    // Polígono 4
+    let poly4 = vec![
+        Vector2::new(413.0, 177.0),
+        Vector2::new(448.0, 159.0),
+        Vector2::new(502.0, 88.0),
+        Vector2::new(553.0, 53.0),
+        Vector2::new(535.0, 36.0),
+        Vector2::new(676.0, 37.0),
+        Vector2::new(660.0, 52.0),
+        Vector2::new(750.0, 145.0),
+        Vector2::new(761.0, 179.0),
+        Vector2::new(672.0, 192.0),
+        Vector2::new(659.0, 214.0),
+        Vector2::new(615.0, 214.0),
+        Vector2::new(632.0, 230.0),
+        Vector2::new(580.0, 230.0),
+        Vector2::new(597.0, 215.0),
+        Vector2::new(552.0, 214.0),
+        Vector2::new(517.0, 144.0),
+        Vector2::new(466.0, 180.0),
+    ];
+
+    // Polígono 5
+    let poly5 = vec![
+        Vector2::new(682.0, 175.0),
+        Vector2::new(708.0, 120.0),
+        Vector2::new(735.0, 148.0),
+        Vector2::new(739.0, 170.0),
+    ];
+
     // Rellenar polígonos con sus colores
     fill_polygon(&mut framebuffer, &poly1, Color::YELLOW, Color::WHITE);
     fill_polygon(&mut framebuffer, &poly2, Color::BLUE, Color::WHITE);
     fill_polygon(&mut framebuffer, &poly3, Color::RED, Color::WHITE);
+    fill_polygon(&mut framebuffer, &poly4, Color::GREEN, Color::WHITE);
+
+    // Agujero en el polígono 4
+    fill_polygon(&mut framebuffer, &poly5, background, Color::WHITE);
 
     // Guardar imagen en formato PNG
     let output_file = "out.png";
